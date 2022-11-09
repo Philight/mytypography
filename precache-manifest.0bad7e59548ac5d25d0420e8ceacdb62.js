4258205d2f781ed8530f26c7dad2a7fb56064827
Philight <Philight@users.noreply.github.com>
2022-11-09 21:10:42 +0100

    Updates

---
+++ precache-manifest.0bad7e59548ac5d25d0420e8ceacdb62.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d19bfab0708229d0a3",
+    "url": "/mytypography/static/css/main.ee3b8479.chunk.css"
+  },
+  {
+    "revision": "53d19bfab0708229d0a3",
+    "url": "/mytypography/static/js/main.53d19bfa.chunk.js"
+  },
+  {
+    "revision": "fe424a2686be241ab5ea",
+    "url": "/mytypography/static/css/1.ec064b72.chunk.css"
+  },
+  {
+    "revision": "fe424a2686be241ab5ea",
+    "url": "/mytypography/static/js/1.fe424a26.chunk.js"
+  },
+  {
+    "revision": "390af42bc8d8d6c9b259",
+    "url": "/mytypography/static/js/runtime~main.390af42b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mytypography/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "10395a32bb2f917a0217d5855c6a16f3",
+    "url": "/mytypography/static/media/minimalistbackground.10395a32.jpg"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/mytypography/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "/mytypography/static/media/Roboto-BlackItalic.99682a78.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/mytypography/static/media/Roboto-BoldItalic.5b44818d.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/mytypography/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/mytypography/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/mytypography/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/mytypography/static/media/Roboto-Italic.a720f17a.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/mytypography/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/mytypography/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/mytypography/static/media/Roboto-MediumItalic.cf23e1bb.ttf"
+  },
+  {
+    "revision": "a3ce4440f2abf76f4a1b14b83920138c",
+    "url": "/mytypography/static/media/Roboto-LightItalic.a3ce4440.ttf"
+  },
+  {
+    "revision": "d7d86e7e7e6fdfcac55037c13bafde3a",
+    "url": "/mytypography/static/media/Galada-Regular.d7d86e7e.ttf"
+  },
+  {
+    "revision": "35a9c89aff1396595ad345e378a32aca",
+    "url": "/mytypography/static/media/Roboto-ThinItalic.35a9c89a.ttf"
+  },
+  {
+    "revision": "0b357f428c4f4fa16003573e079d78ce",
+    "url": "/mytypography/index.html"
+  }
+];
